5d711ce07662b05fcc8d5bd84a0ac8b1237403cb
n1co33 <66425378+n1co33@users.noreply.github.com>
2023-06-08 18:05:31 +0900

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.itheima.reggie.common;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ *
+ * 全局异常处理
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@Slf4j
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    /**
+     *
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        return R.error("发现异常");
+    }
+}
