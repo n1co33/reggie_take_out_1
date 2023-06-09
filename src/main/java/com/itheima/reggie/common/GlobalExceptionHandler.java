81d792057ce4fe1bee5a4076c7af961fe3da868f
n1co33 <66425378+n1co33@users.noreply.github.com>
2023-06-09 10:41:49 +0900

    Update GlobalExceptionHandler.java

--- src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -27,6 +27,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
         log.error(ex.getMessage());
-        return R.error("发现异常");
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
     }
 }
