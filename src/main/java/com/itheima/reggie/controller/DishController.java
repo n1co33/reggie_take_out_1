8f81637af2a9cb45043e81f247ac1f46f39c4ecf
n1co33 <66425378+n1co33@users.noreply.github.com>
2023-06-14 18:23:11 +0900

    菜品功能

---
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -0,0 +1,23 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 菜品管理
+ */
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
+    public R<String> save(){
+        return null;
+    }
+}
