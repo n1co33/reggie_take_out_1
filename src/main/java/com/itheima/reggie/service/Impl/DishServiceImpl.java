a4473da7d4387ae593a81e87ed41c7372a5d9d50
n1co33 <66425378+n1co33@users.noreply.github.com>
2023-06-15 12:01:37 +0900

    菜品口味查询

--- src/main/java/com/itheima/reggie/service/Impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/Impl/DishServiceImpl.java
@@ -1,13 +1,63 @@
 package com.itheima.reggie.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService{
+    @Autowired
+    private DishFlavorService dishFlavorService;
+    /**
+     * 新增菜品，同时保存口味数据
+     * @param dishDto
+     */
+
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+        //保存菜品基本信息到菜品表Dish
+        this.save(dishDto);
+        Long dishId = dishDto.getId();//菜品id
+
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        //为flavors集合中的每一项赋dishId值，并输出集合
+        flavors.stream().map((item)->{
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+        //保存菜品口味数据到菜品口味表dish_flavor
+        dishFlavorService.saveBatch(dishDto.getFlavors());
+    }
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     * @param id
+     * @return
+     */
+    public DishDto getByIdWithFlavor(Long id) {
+        //查询菜品基本信息，从dish表查询
+        Dish dish = this.getById(id);
+        DishDto dishDto = new DishDto();
+        BeanUtils.copyProperties(dish,dishDto);
+        //查询当前菜品的口味信息，从dish_flavor表查询
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId,dish.getId());
+        List<DishFlavor> flavors = dishFlavorService.list(queryWrapper);
+        dishDto.setFlavors(flavors);
+        return dishDto;
+    }
 }
